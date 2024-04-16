46e9c205947318ef72e8545526ffa34cf25e9b52
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-16 12:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+function swapTheme() {
+            var app = document.getElementById('app');
+            var button = document.getElementById('swap');
 
+            if (app.classList.contains('day')) {
+                app.classList.remove('day');
+                app.classList.add('night');
+                button.textContent = 'Day Mode';
+            } else {
+                app.classList.remove('night');
+                app.classList.add('day');
+                button.textContent = 'Night Mode';
+            }
+        }
